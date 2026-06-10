73356dae63bdb444920e9b77e30095b9dffa8ea8
Matthew Morrison <matthew.morrison@savvywealth.com>
2026-06-10 11:06:10 -0400

    change delay to 0 for git blame

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,8 +49,7 @@ local lazy_spec = {
     {
         'lewis6991/gitsigns.nvim',
         opts = {
-            current_line_blame = false,
-            current_line_blame_opts = { delay = 300, virt_text_pos = 'eol' },
+            current_line_blame_opts = { delay = 0 },
         },
         config = function(_, opts)
             require('gitsigns').setup(opts)
